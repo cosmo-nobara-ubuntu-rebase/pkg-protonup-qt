6734b9759c410a7ca52c9e15f5a979532a312582
ferrreo <harderthanfire@gmail.com>
2023-04-24 17:38:25 +0100

    Update to lunar

--- release.sh
+++ release.sh
@@ -6,12 +6,12 @@ dpkg-sig --sign builder ./output/pika-package-manager*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% pika-*-meta*)'
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% pika-package-manager*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-*-meta*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-package-manager*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/pika-*-meta*.deb
-reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/pika-package-manager*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-*-meta*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-package-manager*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
