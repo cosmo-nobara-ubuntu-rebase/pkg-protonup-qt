a85c8e12034d73c9925f854da9b683b0b7f31257
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-08-28 17:04:13 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,17 +1,11 @@
 # Sign the packages
-dpkg-sig --sign builder ./output/pika-*-meta*.deb
-dpkg-sig --sign builder ./output/pika-package-manager*.deb
+dpkg-sig --sign builder ./output/*.deb
 
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
-# Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-*-meta*)'
-reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-package-manager*)'
-
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-*-meta*.deb
-reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-package-manager*.deb
+reprepro -C main -V --basedir ./output/repo/ includedeb lunar ./output/*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
